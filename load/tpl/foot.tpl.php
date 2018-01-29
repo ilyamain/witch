c22dbb35a35bc90fd797eb005f4a9e0857d414bf
Ilya <ilya.neba@gmail.com>
2018-01-29 20:33:18 +0300

    Delete foot.tpl.php

--- load/tpl/foot.tpl.php
+++
@@ -1,5 +0,0 @@
-<?if (!defined('PROGRAM_NAME')) die(); // Защита от прямого вызова скрипта?>
-<div>
-	<span>Repository at: </span>
-	<a href="https://github.com/ilyamain/witch" target="_blank">https://github.com/ilyamain/witch</a>
-</div>
