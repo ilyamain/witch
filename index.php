6aab07cc1cc855ad3e956807d7d5ae160dddacb1
Ilya <ilya.neba@gmail.com>
2018-01-29 20:29:38 +0300

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?
-// Загрузка системных файлов, модулей. Или установка программы
-require_once ($_SERVER[DOCUMENT_ROOT].DIRECTORY_SEPARATOR.'load'.DIRECTORY_SEPARATOR.'start.php');
-// Загрузка шаблона страницы и вывод на экран
-require_once ($page->element['html']);
-?>
