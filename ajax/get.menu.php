f1862ddab844b74714eb4cebebe9b5b5bdd99d92
Ilya <ilya.neba@gmail.com>
2018-01-29 20:21:33 +0300

    Delete get.menu.php

--- ajax/get.menu.php
+++
@@ -1,6 +0,0 @@
-<?define('AJAX_FORM', true);?>
-<?require_once ($_SERVER[DOCUMENT_ROOT].DIRECTORY_SEPARATOR.'load'.DIRECTORY_SEPARATOR.'start.php');?>
-<?
-if (!defined('PROGRAM_NAME')) die(); // Защита от прямого вызова скрипта
-require($page->element['menu']);
-?>
